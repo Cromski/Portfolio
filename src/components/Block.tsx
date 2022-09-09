b7d6852dcececcf7d6107eb87c6c2a9635431a32
Cromski <90246462+Cromski@users.noreply.github.com>
2022-09-09 23:20:24 +0200

    bang

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -6,14 +6,12 @@ interface Props {
     thing: string
 }
 
-const Block: FC<Props> = ({header, date, thing}) => {
-    return(
-        <div className=' mx-auto  w-[30%] mb-7'>
-            <h1 className=' float-left text-3xl ' >{header}</h1>
-            <h1>{date}</h1>
-            <h1>{thing}</h1>
-        </div>
-    )
-}
+const Block: FC<Props> = ({header, date, thing}) => (
+    <div className=' mx-auto  w-[80%] mb-7'>
+        <h1 className=' float-left text-3xl '>{header}</h1>
+        <h1 className=' float-left '   >{date}</h1>
+        <h1>{thing}</h1>
+    </div>
+)
 
 export default Block
--- src/components/Thing.tsx
+++ src/components/Thing.tsx
@@ -6,16 +6,14 @@ interface Props {
     overHeader: string
 }
 
-const Thing: FC<Props> = ({ overHeader }) => {
-    return (
-        <div className=' mt-[3%]'>
-            <h1 className=" text-3xl ">{overHeader}</h1>
-            <div className=' mx-auto'>
-                <Block header={'University'} date={'2020-2023'} thing={'IT University of Copenhagen'} />
-                <Block header={'High school'} date={'2017-2020'} thing={'Sydkysten Gymnasium HTX'} />
-            </div>
+const Thing: FC<Props> = ({ overHeader }) => (
+    <div className=' bg-slate-200 bg-opacity-5 border-2 inline-block w-[50%] rounded-2xl mt-[3%]'>
+        <h1 className=" mt-3 text-3xl ">{overHeader}</h1>
+        <div className=' mx-auto'>
+            <Block header={'University'} date={'2020-2023'} thing={'IT University of Copenhagen'} />
+            <Block header={'High school'} date={'2017-2020'} thing={'Sydkysten Gymnasium HTX'} />
         </div>
-    )
-}
+    </div>
+)
 
 export default Thing
