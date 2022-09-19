ae6c30b597aa65789de71a2d8344e205ca0e6683
Jakob Rasmus Henriksen <jarh@itu.dk>
2022-09-20 01:44:19 +0200

    Update Block.tsx

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -12,7 +12,7 @@ const Block: FC<Props> = ({header, date, children}) => (
         <div className=' mx-auto  w-[80%] mb-5 flex' >
             <div className='flex-col w-1/2 text-right inline-block mr-[5%]'>
                 <h1 className=' text-[#EEE8AA] text-3xl '>{header}</h1>
-                <h1 className='  '>{date}</h1>
+                <h1 className=' italic '>{date}</h1>
                 {/* <div className=' mr-[7%] border-r-2  border-gray-200' >&#8205;</div> */}
             </div>
             <div className=' w-1/2 '>
