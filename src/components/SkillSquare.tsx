b4d3887b8e5d9676136bdc550bd2b4a4741c03ae
Jakob Rasmus Henriksen <jarh@itu.dk>
2022-09-21 00:14:13 +0200

    Added some logos for the skills page, and adjusted some styling

--- src/components/SkillSquare.tsx
+++ src/components/SkillSquare.tsx
@@ -8,8 +8,8 @@ interface Props{
 
 const SkillSquare: FC<Props> = ({skillName, pic, skillRating}) => (
     <div className='inline-block ml-[1%] mr-[1%] mb-[3%] min-w-[150px] w-[18%] content-center bg-slate-200 bg-opacity-5 border-2 rounded-2xl'>
-        <img className=' w-[80%] mx-auto mt-[6%] rounded-lg' src={pic} />
-        <h1 className=' text-[20px] font-medium pt-1 pb-3'>{skillName}</h1>
+        <img className=' w-[85%] mx-auto mt-[6%] rounded-lg' src={pic} />
+        <h1 className=' leading-6 text-[20px] font-medium pt-1 pb-3'>{skillName}</h1>
         {/* <h1 className=' text-sm  italic mb-[3%]'>{skillRating}</h1> */}
     </div>
 )
--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -5,6 +5,18 @@ import PageContext from "../PageContext"
 import SkillSquare from '../components/SkillSquare'
 import TempFacePic from '../assets/IotaHeroes-Logo.jpg'
 import SkillSquareContainer from '../components/SkillSquareContainer'
+import JavaScriptLogo from '../assets/js.png'
+import TypeScriptLogo from '../assets/ts.png'
+import CSharpLogo from '../assets/csharp.png'
+import SQLLogo from '../assets/sql.png'
+import PythonLogo from '../assets/python.png'
+import FSharpLogo from '../assets/fsharp.png'
+import GoLogo from '../assets/go.png'
+import JavaLogo from '../assets/java.png'
+import ReactLogo from '../assets/react.png'
+import TailwindCSSLogo from '../assets/tailwindcss.png'
+import GitLogo from '../assets/git.png'
+import BlazorLogo from '../assets/blazor.png'
 
 const info: Page = {
     miniHeading: "Skills",
@@ -37,22 +49,21 @@ const PageContent = () => {
                     >{info.bigHeading}</h1>
                 <img src={info.svg} alt="svg" className=" w-[80px] mx-auto " />
                 <SkillSquareContainer skillHeader='Languages'>
-                    <SkillSquare skillName='JavaScript' pic={TempFacePic} skillRating='Experienced' />
-                    <SkillSquare skillName='TypeScript' pic={TempFacePic} skillRating='Experienced' />
-                    <SkillSquare skillName='C#' pic={TempFacePic} skillRating='Experienced' />
-                    <SkillSquare skillName='SQL' pic={TempFacePic} skillRating='Experienced' />
-                    <SkillSquare skillName='Python' pic={TempFacePic} skillRating='Experienced' />
-                    <SkillSquare skillName='F#' pic={TempFacePic} skillRating='Experienced' />
-                    <SkillSquare skillName='Go' pic={TempFacePic} skillRating='Experienced' />
-                    <SkillSquare skillName='Java' pic={TempFacePic} skillRating='Experienced' />
+                    <SkillSquare skillName='JavaScript' pic={JavaScriptLogo} skillRating='Experienced' />
+                    <SkillSquare skillName='TypeScript' pic={TypeScriptLogo} skillRating='Experienced' />
+                    <SkillSquare skillName='C#' pic={CSharpLogo} skillRating='Experienced' />
+                    <SkillSquare skillName='SQL' pic={SQLLogo} skillRating='Experienced' />
+                    <SkillSquare skillName='Python' pic={PythonLogo} skillRating='Experienced' />
+                    <SkillSquare skillName='F#' pic={FSharpLogo} skillRating='Experienced' />
+                    <SkillSquare skillName='Go' pic={GoLogo} skillRating='Experienced' />
+                    <SkillSquare skillName='Java' pic={JavaLogo} skillRating='Experienced' />
                 </SkillSquareContainer>
                 
                 <SkillSquareContainer skillHeader='Technologies'>
-                    <SkillSquare skillName='React' pic={TempFacePic} skillRating='Experienced' />
-                    <SkillSquare skillName='TailwindCSS' pic={TempFacePic} skillRating='Experienced' />
-                    <SkillSquare skillName='Git' pic={TempFacePic} skillRating='Experienced' />
-                    <SkillSquare skillName='Materialize' pic={TempFacePic} skillRating='Experienced' />
-                    <SkillSquare skillName='Blazor' pic={TempFacePic} skillRating='Experienced' />
+                    <SkillSquare skillName='React' pic={ReactLogo} skillRating='Experienced' />
+                    <SkillSquare skillName='TailwindCSS' pic={TailwindCSSLogo} skillRating='Experienced' />
+                    <SkillSquare skillName='Git' pic={GitLogo} skillRating='Experienced' />
+                    <SkillSquare skillName='Blazor' pic={BlazorLogo} skillRating='Experienced' />
                 </SkillSquareContainer>
 
                 <SkillSquareContainer skillHeader={`IDE's and editors`}>
