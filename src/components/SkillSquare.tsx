bdefaee64cdb837a6bfc30ec5eea00b3049e9248
Cromski <90246462+Cromski@users.noreply.github.com>
2022-09-21 10:49:15 +0200

    fixed responsiveness on cards having 2 lines of text

--- src/components/SkillSquare.tsx
+++ src/components/SkillSquare.tsx
@@ -9,7 +9,9 @@ interface Props{
 const SkillSquare: FC<Props> = ({skillName, pic, skillRating}) => (
     <div className='inline-block ml-[1%] mr-[1%] mb-[3%] min-w-[150px] w-[18%] content-center bg-slate-200 bg-opacity-5 border-2 rounded-2xl'>
         <img className=' w-[85%] mx-auto mt-[6%] rounded-lg' src={pic} />
-        <h1 className=' leading-6 text-[20px] font-medium pt-1 pb-3'>{skillName}</h1>
+        <div className=' h-16 flex justify-center'>
+            <h1 className=' mt-[8px] inline-block leading-6 text-[20px] font-medium '>{skillName}</h1>
+        </div>
         {/* <h1 className=' text-sm  italic mb-[3%]'>{skillRating}</h1> */}
     </div>
 )
